bed47f633f0d904ed8806883ca94f14d0f4d12b2
Jordana <jordana_buranello@hotmail.com>
2021-12-01 08:41:02 -0300

    Definição de rotas

---
+++ config/index.js
@@ -0,0 +1,4 @@
+const yourName = prompt("Como você se chama?");
+const welcomeUser = document.getElementById("welcome");
+welcomeUser.innerHTML = "<p>Olá, " + yourName + ". Seja bem-vinda ao Trivia Dev! <br> Preparada para jogar?</p>";
+console.log('eu')

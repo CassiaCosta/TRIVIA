2a6bc7905b876e9e76f1c60c70d5a4d6ce7f2ccd
Cássia Costa <cassia.silva93@hotmail.com>
2021-12-01 08:56:25 -0300

    Estruturação das páginas e estilização

--- config/second-subject.js
+++ config/second-subject.js
@@ -6,5 +6,5 @@ function result() {
     if (document.getElementById('correct8').checked) { score++; }
 
     const testeDiv = document.getElementById("testDiv");
-    testeDiv.innerHTML = "<p>sua pontuação é: " + score + "</p>";
+    testeDiv.innerHTML = "<p>sua pontuação é: " + score + "<br> Respostas corretas: <br> 1) head <br>2) body<br> 3) / <br>4) Define a cor do fundo</p>";
 }

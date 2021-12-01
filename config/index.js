99d0e937b877d88fab93a484be14bda88d528122
Cássia Costa <cassia.silva93@hotmail.com>
2021-12-01 12:43:37 -0300

    Formatação dos botões

--- config/index.js
+++ config/index.js
@@ -1,3 +1,3 @@
 const name = prompt("Como você se chama?");
 const welcomeUser = document.getElementById("welcome");
-welcomeUser.innerHTML = "<p>Olá, " + name + ". Seja bem-vinda ao Trivia Dev! <br> Preparada para jogar?</p>";
+welcomeUser.innerHTML = "<p>Olá, " + name + "! Seja bem-vinda ao Trivia Dev! <br> Preparada para jogar?</p>";
